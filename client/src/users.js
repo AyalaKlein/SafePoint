5de4e336482670a8455de4d17129b2614bb84e5b
Eden Sigman <edensigman@gmail.com>
2020-02-15 19:07:37 +0200

    fix users table info

--- client/src/users.js
+++ client/src/users.js
@@ -1,90 +1,82 @@
-import React, { useState, Component } from 'react';
+import React from 'react';
 import MaterialTable from 'material-table';
-import ReactDOM from 'react-dom';
-import axios from 'axios';
 
 const UsersPage = () => {
-    
-    const [state, setState] = React.useState({
-        columns: [
-          { title: 'Username', field: 'Username' },
-          { title: 'Fitness', field: 'Fitness' },
-          { title: 'Age', field: 'Age'},
-        ],
-        data: [
-            { Username: 'Mehmet', Fitness: 8, Age: 63 },
-            {
-                Username: 'Zerya Betül',
-                Fitness: 4,
-                Age: 34,
-            },
-        ],
-        users: [],
-      });
+    const columns = [
+        { title: 'Username', field: 'Username' },
+        { title: 'Fitness', field: 'Fitness' },
+        { title: 'Age', field: 'Age'},
+    ];
 
-      fetch(`http://localhost:${process.env.REACT_APP_SERVER_PORT}/users/getAll`)
-      .then(response => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw new Error('Something went wrong ...');
-        }
-      })
-      .then(data => {
-          console.log(state.data);
-          state.data = data;
-          setState({ users: data });
-          console.log(state.data);
+    const [users, setUsers] = React.useState([]);
+
+    fetch(`http://localhost:${process.env.REACT_APP_SERVER_PORT}/users/getAll`)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error('Something went wrong ...');
+            }
+        })
+        .then(data => {
+            setUsers(data);
         });
-      
+
+    const onRowAddCallback = (newData) => (
+        new Promise(resolve => {
+            setTimeout(() => {
+                resolve();
+                setUsers(prevState => {
+                    // const data = [...prevState.data];
+                    // data.push(newData);
+                    // return { ...prevState, data };
+                });
+            }, 600);
+        })
+    );
+
+    const onRowUpdateCallback = (newData, oldData) => (
+        new Promise(resolve => {
+            setTimeout(() => {
+                resolve();
+                if (oldData) {
+                    // setUsers(prevState => {
+                    //     const data = [...prevState.data];
+                    //     data[data.indexOf(oldData)] = newData;
+                    //     return { ...prevState, data };
+                    // });
+                }
+            }, 600);
+        })
+    );
+
+    const onRowDeleteCallback = (oldData) => (
+        new Promise(resolve => {
+            setTimeout(() => {
+                resolve();
+                // setUsers(prevState => {
+                //     const data = [...prevState.data];
+                //     data.splice(data.indexOf(oldData), 1);
+                //     return { ...prevState, data };
+                // });
+            }, 600);
+        })
+    );
     
     return (
         <div>
-      <MaterialTable
-      title="Editable Example"
-      columns={state.columns}
-      data={state.data}
-      editable={{
-        onRowAdd: newData =>
-          new Promise(resolve => {
-            setTimeout(() => {
-              resolve();
-              setState(prevState => {
-                const data = [...prevState.data];
-                data.push(newData);
-                return { ...prevState, data };
-              });
-            }, 600);
-          }),
-        onRowUpdate: (newData, oldData) =>
-          new Promise(resolve => {
-            setTimeout(() => {
-              resolve();
-              if (oldData) {
-                setState(prevState => {
-                  const data = [...prevState.data];
-                  data[data.indexOf(oldData)] = newData;
-                  return { ...prevState, data };
-                });
-              }
-            }, 600);
-          }),
-        onRowDelete: oldData =>
-          new Promise(resolve => {
-            setTimeout(() => {
-              resolve();
-              setState(prevState => {
-                const data = [...prevState.data];
-                data.splice(data.indexOf(oldData), 1);
-                return { ...prevState, data };
-              });
-            }, 600);
-          }),
-      }}
-    />
+            <MaterialTable
+                title='Editable Example'
+                columns={columns}
+                data={users}
+                editable={{
+                    onRowAdd: onRowAddCallback,
+                    onRowUpdate: onRowUpdateCallback,
+                    onRowDelete: onRowDeleteCallback,
+                }}
+            />
         </div>
     );
-
 };
  
 export default UsersPage;
