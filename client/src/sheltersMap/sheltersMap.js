afb42a63068d83c442310a9136e40423e948ca73
Gil Mishal <gilmishal@gmail.com>
2020-05-26 23:29:50 +0300

    partly moved adding shelters to admin

--- client/src/sheltersMap/sheltersMap.js
+++ client/src/sheltersMap/sheltersMap.js
@@ -4,6 +4,22 @@ import GoogleMapReact from 'google-map-react';
 import { IconButton } from 'office-ui-fabric-react/lib/Button';
 import { Icon } from 'office-ui-fabric-react/lib/Icon';
 import { InfoWindow } from 'google-map-react';
+import MaterialTable from 'material-table';
+import AddBox from '@material-ui/icons/AddBox';
+import ArrowDownward from '@material-ui/icons/ArrowDownward';
+import Check from '@material-ui/icons/Check';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import Clear from '@material-ui/icons/Clear';
+import DeleteOutline from '@material-ui/icons/DeleteOutline';
+import Edit from '@material-ui/icons/Edit';
+import FilterList from '@material-ui/icons/FilterList';
+import FirstPage from '@material-ui/icons/FirstPage';
+import LastPage from '@material-ui/icons/LastPage';
+import Remove from '@material-ui/icons/Remove';
+import SaveAlt from '@material-ui/icons/SaveAlt';
+import Search from '@material-ui/icons/Search';
+import ViewColumn from '@material-ui/icons/ViewColumn';
 
 const mapStyles = {
   width: '100%',
@@ -27,15 +43,76 @@ class MapContainer extends Component {
   }
 
   componentDidMount() {
-    fetch(`http://localhost:${process.env.REACT_APP_SERVER_PORT}/shelters/getall`)
-    .then(res => res.json())
-    .then(result => {
-      console.log(result)
-      this.setState({
-        shelters: result
-      })
-    })
+    this.fetchData()
   }
+
+  fetchData() {
+
+    fetch(`http://localhost:${process.env.REACT_APP_SERVER_PORT}/shelters/getAll`)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error('Something went wrong ...');
+        }
+      })
+      .then(data => {
+        this.setState({ shelters: data });
+      })
+  }
+
+  onRowAddCallback = (newData) => {
+    return new Promise(resolve => {
+      fetch(`http://localhost:${process.env.REACT_APP_SERVER_PORT}/shelters/create`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newData)
+      })
+        .then(response => {
+          if (response.ok) {
+            resolve();
+          } else {
+            throw new Error('Something went wrong ...');
+          }
+        })
+    });
+  };
+
+  onRowUpdateCallback = (newData, oldData) => {
+    return new Promise(resolve => {
+      fetch(`http://localhost:${process.env.REACT_APP_SERVER_PORT}/shelters/edit`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newData)
+      })
+        .then(response => {
+          if (response.ok) {
+            this.fetchData();
+            resolve();
+          } else {
+            throw new Error('Something went wrong ...');
+          }
+        })
+    });
+  };
+
+  onRowDeleteCallback = (oldData) => {
+    return new Promise(resolve => {
+      fetch(`http://localhost:${process.env.REACT_APP_SERVER_PORT}/shelters/delete/${oldData.Id}`)
+        .then(response => {
+          if (response.ok) {
+            this.fetchData();
+            resolve();
+          } else {
+            throw new Error('Something went wrong ...');
+          }
+        })
+    });
+  };
 
   shelterClicked(index, elem) {
     console.log(elem)
@@ -44,7 +121,7 @@ class MapContainer extends Component {
     });
   }
 
-  mapClicked(a,b) {
+  mapClicked(a, b) {
     this.setState({
       clickedShelter: null
     })
@@ -53,17 +130,24 @@ class MapContainer extends Component {
   displayMarkers = () => {
     return this.state.shelters.map((shelter, index) => {
       return (
-          <Marker key={shelter.Id}
-                lat={parseFloat(shelter.LocY)}
-                lng={parseFloat(shelter.LocX)}
-                onClick={this.shelterClicked}>
-          </Marker>
+        <Marker key={shelter.Id}
+          lat={parseFloat(shelter.LocY)}
+          lng={parseFloat(shelter.LocX)}
+          onClick={this.shelterClicked}>
+        </Marker>
       )
     })
   }
 
   render() {
     const { isExpanded } = this.state
+
+    let lookupOptions = {};
+    const columns = [
+      { title: 'Description', field: 'Description' },
+      { title: 'MaxPopulation', field: 'MaxPopulation', type: 'numeric', lookup: lookupOptions }
+    ];
+    const { shelters, shelterChangesByMonth, sheltersCountByPopulation } = this.state
     console.log(isExpanded)
     let defaultProps = {
       center: { lat: 47.444, lng: -122.176 },
@@ -71,8 +155,41 @@ class MapContainer extends Component {
     };
 
     return (
-      <div className="position-relative h-100 w-100 d-flex flex-row">
-        <div className="position-relative" style={{width:isExpanded? `${sideViewSize}px`:'0px'}}>
+      <div className="position-relative d-flex flex-row">
+        <div className="position-relative" style={{ width: isExpanded ? `initial` : '0px' }}>
+
+          <MaterialTable
+            title={null}
+            columns={columns}
+            data={shelters}
+            editable={{
+              onRowAdd: this.onRowAddCallback,
+              onRowUpdate: this.onRowUpdateCallback,
+              onRowDelete: this.onRowDeleteCallback
+            }}
+            icons={{
+              Add: () => <AddBox />,
+              Check: () => <Check />,
+              Clear: () => <Clear />,
+              Delete: () => <DeleteOutline />,
+              DetailPanel: () => <ChevronRight />,
+              Edit: () => <Edit />,
+              Export: () => <SaveAlt />,
+              Filter: () => <FilterList />,
+              FirstPage: () => <FirstPage />,
+              LastPage: () => <LastPage />,
+              NextPage: () => <ChevronRight />,
+              PreviousPage: () => <ChevronLeft />,
+              ResetSearch: () => <Clear />,
+              Search: () => <Search />,
+              SortArrow: () => <ArrowDownward />,
+              ThirdStateCheck: () => <Remove />,
+              ViewColumn: () => <ViewColumn />,
+            }}
+            options={{
+              filtering: true
+            }}
+          />
           <IconButton className="side-button ms-depth-16" styles={{ icon: { fontSize: '10px' } }} onClick={() => this.setState({ isExpanded: !isExpanded })} iconProps={{ iconName: isExpanded ? 'FlickRight' : 'FlickLeft' }} title="Toggle side panel" ariaLabel="Add" />
         </div>
         <div style={{ height: `calc(100vh - ${topViewSize}px)`, width: isExpanded ? `calc(100vw - ${sideViewSize}px)` : '100vw' }}>
